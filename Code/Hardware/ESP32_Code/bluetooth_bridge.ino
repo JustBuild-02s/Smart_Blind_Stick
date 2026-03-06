535ccc8e4dd804c4cd87518fd1c863db6868fa08
Rahul jana <rj624233@gmail.com>
2026-03-06 12:27:55 +0530

    Update bluetooth_bridge.ino

--- Code/Hardware/ESP32_Code/bluetooth_bridge.ino
+++ Code/Hardware/ESP32_Code/bluetooth_bridge.ino
@@ -2,22 +2,43 @@
 
 BluetoothSerial SerialBT;
 
-void setup()
-{
-Serial.begin(9600); // from Arduino
-SerialBT.begin("SmartBlindStick");
+// ESP32 UART pins
+#define RXD2 16
+#define TXD2 17
 
-Serial.println("Bluetooth Ready");
+void setup() {
+
+  Serial.begin(115200);          // Serial Monitor
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);   // Arduino communication
+
+  SerialBT.begin("SmartBlindStick");   // Bluetooth name
+
+  Serial.println("System Ready");
+  Serial.println("Bluetooth device name: SmartBlindStick");
 }
 
-void loop()
-{
-if(Serial.available())
-{
-String data = Serial.readStringUntil('\n');
+void loop() {
 
-SerialBT.println(data);
+  // Read data coming from Arduino
+  if (Serial2.available()) {
 
-Serial.println(data);
-}
+    String data = Serial2.readStringUntil('\n');
+
+    // Send to phone via Bluetooth
+    SerialBT.println(data);
+
+    // Print in serial monitor for debugging
+    Serial.println(data);
+  }
+
+  // If phone sends something back (optional)
+  if (SerialBT.available()) {
+
+    String phoneData = SerialBT.readStringUntil('\n');
+
+    Serial2.println(phoneData);   // Send back to Arduino if needed
+
+    Serial.println("From Phone: " + phoneData);
+  }
+
 }
